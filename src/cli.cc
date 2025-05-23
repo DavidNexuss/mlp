4a0c81d7af7b3e6345ef85563ae9349397259bf7
DavidNexuss <davidgt7d1@gmail.com>
2025-05-23 14:36:08 +0200

    Fixes autoencoder

--- src/cli.cc
+++ src/cli.cc
@@ -3,6 +3,7 @@
 
 //XOR test suite backpropagator
 void xortest() {
+  printf("================[XOR TEST]========================\n");
   OptimizerCreateInfo optInfo;
   optInfo.learningRate = 0.1f;
   optInfo.momentum     = 0.9f;
@@ -41,31 +42,35 @@ void xortest() {
     net->Propagate(ds->inputs[i], output);
     std::cout << "Input: " << ds->inputs[i] << " " << output << std::endl;
   }
+
+  printf("\n");
 }
 
 void autoencoder() {
+  printf("================[AUTOENCODER]========================\n");
   OptimizerCreateInfo optInfo;
   optInfo.learningRate = 0.1f;
-  optInfo.momentum     = 0.5f;
+  optInfo.momentum     = 0.9f;
   optInfo.function     = MLP_OPTIMIZER_SGD_MOMENTUM;
 
   std::shared_ptr<MLP> net = std::shared_ptr<MLP>(mlpCreate(2));
 
-  net->AddLayer(8, MLP_ACTIVATION_RELU, MLP_INITIALIZE_HE);
-  net->AddLayer(2, MLP_ACTIVATION_RELU, MLP_INITIALIZE_HE);
-  net->AddLayer(8, MLP_ACTIVATION_RELU, MLP_INITIALIZE_HE);
-  net->AddLayer(2, MLP_ACTIVATION_SIGMOID, MLP_INITIALIZE_xAVIER);
+  net->AddLayer(10, MLP_ACTIVATION_RELU);
+  net->AddLayer(2, MLP_ACTIVATION_RELU);
+  net->AddLayer(10, MLP_ACTIVATION_RELU);
+  net->AddLayer(3, MLP_ACTIVATION_SIGMOID);
 
   net->SetOptimizer(optInfo);
+  net->Initialize(MLP_INITIALIZE_RANDOM);
 
   std::shared_ptr<DataSet>
     ds = std::make_shared<DataSet>();
 
   ds->inputs = {
-    {0.0f, 0.0f},
-    {0.0f, 1.0f},
-    {1.0f, 0.0f},
-    {1.0f, 1.0f}};
+    {0.0f, 0.0f, 1.0f},
+    {0.0f, 1.0f, 1.0f},
+    {1.0f, 0.0f, 1.0f},
+    {1.0f, 1.0f, 0.0f}};
 
   ds->targets = ds->inputs;
 
@@ -82,6 +87,7 @@ void autoencoder() {
     net->Propagate(ds->inputs[i], output);
     std::cout << "Input: " << ds->inputs[i] << " " << output << std::endl;
   }
+  printf("\n");
 }
 
 int main() {
